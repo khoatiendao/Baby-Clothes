4e8812030548f49f3000f905a9e2eface0155010
khoatiendao <88887127+khoatiendao@users.noreply.github.com>
2023-07-26 02:42:20 +0700

    3h sang

--- server/src/index.js
+++ server/src/index.js
@@ -31,15 +31,16 @@ app.use('/api/customer', require('./api/customer.js'));
 //Deployment
 const path = require('path');
 // '/admin' serve the files at client-admin/build/* as static files
-app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
-app.get('admin/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'))
+app.use('/admin', express.static(path.resolve(__dirname, '../../client-admin/build')));
+app.get('/admin/*', (req, res) => {
+  console.log("1");
+  res.sendFile(path.resolve(__dirname, '../../client-admin/build', 'index.html'))
 });
 // app.get('/', (req, res) => {
 //   res.send("Hello");
 // });
 // '/' serve the files at client-customer/build/* as static files
-app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
+app.use('/', express.static(path.resolve(__dirname, '../../client-customer/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../../client-customer/build', 'index.html'));
 });
